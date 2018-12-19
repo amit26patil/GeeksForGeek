6c8bc1b8046dcd201743cc2ecafb8624af13657c
Amit Patil <amit26patil@gmail.com>
2018-12-19 10:06:49 +0530

    Median In Stream Done

--- GeeksForGeek/Heap/MedianInStream.cs
+++ GeeksForGeek/Heap/MedianInStream.cs
@@ -12,12 +12,61 @@ namespace GeeksForGeek.Heap
         {
             var leftHeap = new Heap(Heap.Type.Max);
             var rightHeap = new Heap(Heap.Type.Min);
-
-            leftHeap.AddNum(5);
-            leftHeap.AddNum(10);
-            leftHeap.AddNum(3);
             var top = leftHeap.GetTop();
+            var array = new int[] { 2, 8, 3, 5, 16, 11, 15 };
+            leftHeap.AddNum(array[0]);
+            double median = array[0];
+            Console.WriteLine(median);
+            for (int i = 1; i < array.Length; i++)
+            {
+                var leftLength = leftHeap.Count;
+                var rightLength = rightHeap.Count;
+                var num = array[i];
+                if (num < median)
+                {
+                    //Push To left
+                    if (leftLength > rightLength)
+                    {
+                        //extract from left and push to right
+                        var topNum = leftHeap.ExtractTop();
+                        rightHeap.AddNum(topNum);
+                        //then push num to left
+                        leftHeap.AddNum(num);
+                    }
+                    else
+                    {
+                        //push num to left
+                        leftHeap.AddNum(num);
+                    }
+                }
+                else
+                {
+                    //push to left
+                    if (leftLength < rightLength)
+                    {
+                        var topNum = rightHeap.ExtractTop();
+                        leftHeap.AddNum(topNum);
+                        rightHeap.AddNum(num);
+                    }
+                    else
+                    {
+                        rightHeap.AddNum(num);
+                    }
+                }
+                median = GetMedian(leftHeap, rightHeap);
+                Console.WriteLine(median);
+            }
 
+        }
+
+        private static double GetMedian(Heap leftHeap, Heap rightHeap)
+        {
+            if (leftHeap.Count == rightHeap.Count)
+                return (double)(leftHeap.GetTop() + rightHeap.GetTop()) / 2;
+            else if (leftHeap.Count > rightHeap.Count)
+                return leftHeap.GetTop();
+            else
+                return rightHeap.GetTop();
         }
     }
     public class Heap
@@ -35,6 +84,10 @@ namespace GeeksForGeek.Heap
             Min,
             Max
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public int GetTop()
         {
             if (array.Count > 0)
@@ -43,10 +96,26 @@ namespace GeeksForGeek.Heap
             }
             return -1;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
         public void AddNum(int num)
         {
             array.Add(num);
             Heapify(array.Count-1);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int ExtractTop()
+        {
+            Swap(0, array.Count-1);
+            var temp = array[array.Count - 1];
+            array.RemoveAt(array.Count - 1);
+            Heapify(array.Count - 1);
+            return temp;
         }
         public int Count
         {
